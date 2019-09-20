4d05378fc3a3fc054d2f664b17c1b606e0a8552a
Brittany Cryderman [Student] <BCryderman19@Student.SL.On.Ca>
2019-09-20 13:04:28 -0400

    pathfinding

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -6,8 +6,10 @@ using UnityEngine.AI;
 public class Alien : MonoBehaviour
 {
     public Transform target; //where alien should go
+    public float navigationUpdate; //m/s for when alien updates its path
 
     private NavMeshAgent agent;
+    private float navigationTime = 0; // tracks how much time has passed
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +21,12 @@ public class Alien : MonoBehaviour
     {
         if (target != null)
         {
-            agent.destination = target.position;
+            navigationTime += Time.deltaTime; //how much time has passed then updates 
+            if (navigationTime > navigationUpdate)
+            {
+                agent.destination = target.position;
+                navigationTime = 0;
+            }
         }
     }
 }
